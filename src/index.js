7675544e80e5cb3ed5eb390d6f978db65e287f8a
Dfill-16 <davidaso26@gmail.com>
2025-05-21 15:18:36 -0500

    Actualización de ejercicios

--- src/index.js
+++ src/index.js
@@ -1,19 +1,75 @@
-function promesa() {
-  return new Promise((resolve)=>{
+// function promesa() {
+//   return new Promise((resolve)=>{
+//     setTimeout(() => {
+//       resolve('Ejecutada con exito después de 8 segundos')
+//     }, 8000);
+//   })
+// }
+
+// async function ejecutar(){
+//   try {
+//     console.log("Ejecutando...")
+//     const resultado = await promesa()
+//     console.log(resultado)
+//   } catch (error) {
+//     console.log("Fallo en la promesa")
+//   }
+// }
+
+// ejecutar()
+
+// const bol = true
+
+// function promesa(bol) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (!bol) {
+//         resolve('Ejecutada con exito después de 3 segundos')
+//       }
+//       else {
+//         reject('Fallo en la promesa')
+//       }
+//     }, 3000);
+//   })
+// }
+
+// async function ejecutar(bol) {
+//   try {
+//     console.log("Ejecutando..")
+//     const resultado = await promesa(bol)
+//     console.log(resultado)
+//   } catch (error) {
+//     const resultado = await promesa(bol)
+//     console.log(resultado)
+//   }
+// }
+
+// ejecutar(bol)
+
+const num = Math.random()
+console.log(num)
+
+function Aleatorio(number){
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('Ejecutada con exito después de 8 segundos')
-    }, 8000);
+      if(number >= 0.5){
+        resolve('El numero es mayor a 0.5, se ejecutó')
+      } else{
+        reject('La promesa falló')
+      }  
+    }, 3000);
   })
 }
 
-async function ejecutar(){
+async function ejecutar(number){
   try {
-    console.log("Ejecutando...")
-    const resultado = await promesa()
+    console.log("Ejecutando")
+    const resultado = await Aleatorio(number)
     console.log(resultado)
   } catch (error) {
-    console.log("Fallo en la promesa")
+    const resultado = await Aleatorio(number)
+    console.log(resultado)
   }
 }
 
-ejecutar()
+ejecutar(num)
