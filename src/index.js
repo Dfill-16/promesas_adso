63d0d68b3b3e0c338fd72eb29aa18c77f3e4f131
Dfill-16 <davidaso26@gmail.com>
2025-05-21 14:15:16 -0500

    Configuración de promesas completo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function descargar() {
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve("📁 archivo descargado correctamente")
+    },2000)
+  })
+}
+
 async function ejecutar() {
   try {
     console.log("📁 Descarga en proceso...")
-    const respuesta = await 
+    const respuesta = await descargar()
+    console.log(respuesta)
   } catch (error) {
-    
+    console.error("❌ Error al descargar archivo: ",error)
   }
 }
+
+ejecutar()
